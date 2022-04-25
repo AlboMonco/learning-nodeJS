7a93a3c2f4879cbd5ac32f7147ece271d8ef8855
AlboMonco <albomonaco@gmail.com>
2022-04-25 08:33:33 -0300

    Added fetch for check status code from URLs

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,7 +1,19 @@
+import fetch from "node-fetch";
+
+async function checaStatus(arrayURLs) {
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url);
+        return res.status;
+    }));
+    return arrayStatus;
+}
+
 function geraArrayDeURLs(arrayLinks){
     return arrayLinks.map(objeto => Object.values(objeto).join());
 }
 
-export function validaURLs(arrayLinks) {
-    return geraArrayDeURLs(arrayLinks);
+export async function validaURLs(arrayLinks) {
+    const links = geraArrayDeURLs(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    return statusLinks;
 }
